c613d0a3f26c871029dc1fe38fc0a6d09ee75392
VagnerSousa <vagner.s.sousa@outlook.com>
2024-09-22 19:08:16 -0300

    DesafioAPIViaCep

---
+++ src/br/com/desafioApiViaCep/modelos/BuscaCep.java
@@ -0,0 +1,14 @@
+package br.com.desafioApiViaCep.modelos;
+
+public class BuscaCep {
+    private int cep;
+
+    public BuscaCep(int cep) {
+        this.cep = cep;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+}
---
+++ src/br/com/desafioApiViaCep/resolucao/Endereco.java
@@ -0,0 +1,4 @@
+package br.com.desafioApiViaCep.resolucao;
+
+public record Endereco(String cep, String logradouro, String complemento, String Bairro, String localidade, String uf) {
+}
